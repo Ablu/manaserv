7a4cbaebdda5931cb3d07cb4b0877fa99ab6abcb
Erik Schilling <ablu.erikschilling@gmail.com>
2016-07-13 15:28:16 +0200

    install too

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo yum -y install epel-release
 sudo yum install -y cmake make libxml2-devel physfs-devel gcc-c++ libsigc++20-devel sqlite-devel lua-devel || exit 1
-cmake . || exit 1
+cmake -DCMAKE_INSTALL_PREFIX=/usr/ . || exit 1
 make -j$(nproc) || exit 1
-
+sudo make install
