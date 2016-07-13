922af96131fa5035c0e662cd3164e6a46e6894ef
Erik Schilling <ablu.erikschilling@gmail.com>
2016-07-13 15:00:33 +0200

    Added sudo to yum commands

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-yum -y install epel-release
-yum install -y cmake make libxml2-devel physfs-devel gcc-c++ libsigc++20-devel sqlite-devel lua-devel || exit 1
+sudo yum -y install epel-release
+sudo yum install -y cmake make libxml2-devel physfs-devel gcc-c++ libsigc++20-devel sqlite-devel lua-devel || exit 1
 cmake . || exit 1
 make -j$(nproc) || exit 1
 
